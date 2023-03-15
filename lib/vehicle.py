e6363fa3ed9b1803ba363f3fa826e1a96eafa4bf
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-15 09:57:31 -0700

    Completed Code Along

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,17 @@
 class Vehicle:
-    pass
+
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        print("vrrrrrrrooom!")
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        print("filling up!")
+        return "filling up!"
+
+# fiat = Vehicle("small", 4)
+# fiat.go()
+# fiat.fill_up_tank()
